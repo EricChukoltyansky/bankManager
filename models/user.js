540b25209039c32a88981e2504bc3858d9f6d680
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-20 22:06:19 +0200

    getUsers

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 13,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+    validate(value) {
+      if (!validator.isMobilePhone(value, "he-IL")) {
+        throw new Error("Invalid phone number");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email");
+      }
+    },
+  },
+});
